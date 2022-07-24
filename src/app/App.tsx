0a0b4457f56dd8bc0fe71b94b9c57ca6eb6ad423
dalissongabriel <alissongabrieldino@gmail.com>
2022-07-24 17:23:39 -0300

    feat: :art: improves using react router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,36 @@
+import {
+  Box,
+  Center,
+  Divider,
+  Flex,
+  List,
+  ListItem,
+  Text,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { ROUTES } from "../constants/RoutesConstants";
+
 function App() {
   return (
-    <div className="App">
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Center h={"100vh"}>
+      <Flex direction={"column"} rowGap={30}>
+        <Text fontSize="3xl" fontWeight={"bold"}>
+          Chakra-UI Examples
+        </Text>
+
+        <Box p="5" maxW="320px" borderWidth="1px">
+          <List display={"flex"} flexDir="column" as={"nav"}>
+            <ListItem _hover={{ color: "blue.300", fontWeight: "semibold" }}>
+              <Link to={ROUTES.CARD}>Card</Link>
+            </ListItem>
+            <Divider my={2} />
+            <ListItem _hover={{ color: "blue.300", fontWeight: "semibold" }}>
+              <Link to={ROUTES.FORM}>Form</Link>
+            </ListItem>
+          </List>
+        </Box>
+      </Flex>
+    </Center>
   );
 }
 
--- src/app/root.tsx
+++ src/app/root.tsx
@@ -1,12 +1,21 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Example from "../components/CardExample";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CardScreen from "../routes/examples/card";
+import FormScreen from "../routes/examples/form";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
-      <Example />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="examples/card" element={<CardScreen />} />
+          <Route path="examples/form" element={<FormScreen />} />
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
 );
